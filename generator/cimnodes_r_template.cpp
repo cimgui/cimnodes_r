7579f27eee8fbeb5a9096fa7612b3e57f0bf3c7b
Victor Bombi <sonoroster@gmail.com>
2020-10-28 15:06:31 +0100

    initial commit

---
+++ generator/cimnodes_r_template.cpp
@@ -0,0 +1,10 @@
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "./ImNodes/ImNodesEz.h"
+#include "cimnodes_r.h"
+
+
+
+#include "auto_funcs.cpp"
+
+
---
+++ generator/output/typedefs_dict.lua
@@ -0,0 +1,7 @@
+local defs = {}
+defs["CanvasState"] = "struct CanvasState"
+defs["SlotInfo"] = "struct SlotInfo"
+defs["_CanvasStateImpl"] = "struct _CanvasStateImpl"
+defs["style_"] = "struct style_"
+
+return defs
