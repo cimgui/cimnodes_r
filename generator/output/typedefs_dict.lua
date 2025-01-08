89961cd3848eae4b2e9f5568cfe279b10749baec
sonoro1234 <sonoroster@gmail.com>
2025-01-08 10:56:04 +0100

    new serializer

--- generator/output/typedefs_dict.lua
+++ generator/output/typedefs_dict.lua
@@ -1,9 +1,8 @@
-local defs = {}
-defs["CanvasState"] = "struct CanvasState"
-defs["CanvasStyle"] = "struct CanvasStyle"
-defs["Context"] = "struct Context"
-defs["SlotInfo"] = "struct SlotInfo"
-defs["StyleVars"] = "struct StyleVars"
-defs["_CanvasStateImpl"] = "struct _CanvasStateImpl"
-
-return defs
+local t={
+  CanvasState="struct CanvasState",
+  CanvasStyle="struct CanvasStyle",
+  Context="struct Context",
+  SlotInfo="struct SlotInfo",
+  StyleVars="struct StyleVars",
+  _CanvasStateImpl="struct _CanvasStateImpl"}
+ return t
